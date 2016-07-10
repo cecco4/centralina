bb0d26ab50723b5dccf5bc1abd3a6e2c837e4b51
Francesco Gatti <gattifrancesco@hotmail.it>
2016-07-10 16:19:48 +0200

    different timing system

--- centralina.ino
+++ centralina.ino
@@ -13,24 +13,30 @@ const int STEP_B = 3;     //pulsante step
 const int START_B = 2;    //pulsante start
 const int REC_B = 4;      //switch recupero
 
-const unsigned long long int INDOOR_TIME = 3000LL;    // 1 min e 30 sec
-const unsigned long long int OUTDOOR_TIME = 150000LL;  // 2 min e 30 sec
+const int INDOOR_TIME = 90*4;    // 1 min e 30 sec
+const int OUTDOOR_TIME = 150*4;  // 2 min e 30 sec
 
-const unsigned long long int yellowTime = 2000LL;     // tempo giallo: 30 sec
-unsigned long long int greenTime = OUTDOOR_TIME;                  // tempo verde: da assegnare 
+const int yellowTime = 30*4;     // tempo giallo: 30 sec
+int greenTime = OUTDOOR_TIME;                  // tempo verde: da assegnare 
+const int preTime = 10*4; //tempo prima dello start: 10 sec
 
-const unsigned long long int preTime = 3000LL; //tempo prima dello start: 10 sec
+int secs = 0; //timer
 
 int greenID=-1, yellowID=-1, redID=-1; // ID per il timer
 
-const unsigned long long int BEEP_TIME = 500LL;      //tempo sirena: 0.5 sec
-const unsigned long long int BEEP_INTERVAL = 1000LL; //intervallo sirena: 1 sec
+const int BEEP_TIME = 500;      //tempo sirena: 0.5 sec
+const int BEEP_INTERVAL = 1000; //intervallo sirena: 1 sec
 
-const int IDLE_STATE = 0;    
-const int ABCICLE_STATE = 1;
-const int CDCICLE_STATE =2;
+const int RED_STATE = 0;    
+const int PRE_STATE = 1;
+const int GREEN_STATE = 2;
+const int YELLOW_STATE = 3;
 int state;  //stato della macchina
-boolean RED_STATE=0, YELLOW_STATE=0, GREEN_STATE=0, BEEP_STATE=0;
+
+const int IDLE_CYCLE = 0;    
+const int AB_CYCLE = 1;
+const int CD_CYCLE = 2;
+int cycle = IDLE_CYCLE;
 
 boolean recupero = false; //stato recupero
 
@@ -43,22 +49,23 @@ void setup() {
   initButton(REC_B);
   
   //stato iniziale 
-  state = IDLE_STATE;
-  toRed();
-  
+  state = RED_STATE;
+  cycle = IDLE_CYCLE;
+    
   // Inizializzo porta seriale
   Serial.begin(9600);  
   Serial.println(F("Centralina avviata"));
+
+  t.every(250, tick);
 }
 
 void loop() {
   //aggiorno timer
   t.update();
 
-  digitalWrite(GREEN_R, !GREEN_STATE); 
-  digitalWrite(YELLOW_R, !YELLOW_STATE); 
-  digitalWrite(RED_R, !RED_STATE);
-  digitalWrite(BEEP_R, !BEEP_STATE);
+  digitalWrite(GREEN_R, state != GREEN_STATE); 
+  digitalWrite(YELLOW_R, state != YELLOW_STATE); 
+  digitalWrite(RED_R, state != RED_STATE && state != PRE_STATE);
 
   
   //NB: il digital read dei bottoni restituisce: 
@@ -76,27 +83,24 @@ void loop() {
 
   }    
   
-  //controllo pressione bottone start
-  startButtonCtrl();
-  
   switch(state) {
-   case IDLE_STATE:
-     toRed(); 
-      
+   case RED_STATE:
+     red(); 
      break;
      
-   case ABCICLE_STATE:
-     //controllo pressione bottone step
-     stepCtrl();        
+   case PRE_STATE:
+     pre();       
      break;
      
      
-   case CDCICLE_STATE:
-     //controllo pressione bottone step
-     stepCtrl();
+   case GREEN_STATE:
+     green();
      break;
+
+   case YELLOW_STATE:
+     yellow();
+     break;   
   }
-  
 
 }
 
@@ -112,110 +116,106 @@ void initRele() {
   }
 }
 
+void red() {
 
-void toGreen() {
- //accende il verde
- GREEN_STATE  = 1;
- YELLOW_STATE = 0;
- RED_STATE    = 0;
- 
- //esegue toYellow() dopo il tempo assegnato al verde
- yellowID = t.after(greenTime, toYellow);
- 
- if(greenTime == 90000LL)
-  Serial.println(F("greentime IN"));
- else if(greenTime == 150000LL)
-  Serial.println(F("greentime OUT"));
- else
-  Serial.println(F("greentime unknow"));
+  if(cycle == IDLE_CYCLE) {
+    //controllo pressione bottone start
+    if(startButtonCtrl()) {
+      
+      if(digitalRead(REC_B)) {
+        Serial.println("START");
+        cycle = AB_CYCLE;
+        state = PRE_STATE;
+        secs = 0;
+        beep(2);      
+       
+      } else {
+        //richiesta recupero accettata
+        Serial.println("START CON RECUPERO");
+        recupero = true;
+        cycle = CD_CYCLE;
+        state = PRE_STATE;
+        secs = 0;
+        beep(2); 
+      }
+    }
+    
+  } else if(cycle == AB_CYCLE) {
+      cycle = CD_CYCLE;
+      state = PRE_STATE;
+      secs = 0;
+      beep(2);
+      
+  } else if(cycle == CD_CYCLE) {
+      cycle = IDLE_CYCLE;
+      state = RED_STATE;
+      secs = 0;
 
- beep(1);
+      if(!recupero)
+        beep(3);
+      else
+        beep(4);
+      recupero = false;
+  }
 }
 
-void toYellow() {
- //accende il giallo
- GREEN_STATE  = 0;
- YELLOW_STATE = 1;
- RED_STATE    = 0; 
-
- //esegue toRed() dopo il tempo assegnato al giallo
- redID = t.after(yellowTime, toRed); 
- if(redID == -1)
-  Serial.println(F("yellow time error"));
- else
-  Serial.println(F("yellow time"));
+void pre() {
+  
+  if(secs > preTime) {
+    state = GREEN_STATE;
+    secs = 0;
+    beep(1);
+  }
 }
 
-void toRed() {
- //accende il rosso
- GREEN_STATE  = 0;
- YELLOW_STATE = 0;
- RED_STATE    = 1; 
-
- if(state == ABCICLE_STATE) {
-   //esegue toGreen() dopo il tempo pre inizio tiri
-   greenID = t.after(preTime, toGreen);
-   if(greenID == -1)
-    Serial.println(F("pre time error"));
-   else
-    Serial.println(F("pre time"));
-   beep(2);
-
-   //passa allo stato CD
-   state = CDCICLE_STATE;
-   
- } else if ( state == CDCICLE_STATE) {
-   //controlla se c'è richiesta di recupero
-   if(!digitalRead(REC_B)) {
-     //richiesta recupero 4 beep 
-     beep(4);  
-   } else {
-     beep(3);
-   }
-   //passo allo stato di attesa
-   state = IDLE_STATE;
- }
+void green() {
+  
+  if(secs > greenTime) {
+    state = YELLOW_STATE;
+    secs = 0;
+    
+  } else if(stepCtrl()) {
+    state = RED_STATE;
+    secs = 0;    
+  }
 }
+
+void yellow() {
+  
+  if(secs > yellowTime || stepCtrl()) {
+    state = RED_STATE;
+    secs = 0;
+  }
+}
+
+
 
 //esegue un beep "count" volte
 void beep(int count) {
-  
+  Serial.print(count, DEC);
+  Serial.println(" beep");
+
+  if(count > 4)
+    count == 4;
+
   if(count > 0)
     startBeep();
   
-  if(count >1) 
+  if(count >1)
     t.every(BEEP_INTERVAL, startBeep, count-1);
 }
 
 void startBeep() {
-  
-  BEEP_STATE = 1;
-  if(t.after(BEEP_TIME, stopBeep) == -1)
-   Serial.println(F("start error"));
-  else
-  Serial.println(F("start beep"));
+  digitalWrite(BEEP_R, LOW);
+  t.after(BEEP_TIME, stopBeep);
 }
 
 void stopBeep() {
-  Serial.println(F("stop beep"));
-  BEEP_STATE = 0;
-}
-
-void stopTimers() {
- if(redID >=0)
-  t.stop(redID);
- 
- if(yellowID >=0)
-  t.stop(yellowID);
- 
- if(greenID >=0)
-  t.stop(greenID);
- 
- greenID = yellowID = redID = -1; 
+  digitalWrite(BEEP_R, HIGH);
 }
 
 
-void stepCtrl() {
+boolean stepCtrl() {
   //il debounce evita paciughi nella lettura della pressione del bottone
   static int deb =0;
   static boolean startPush = false; 
@@ -233,15 +233,15 @@ void stepCtrl() {
    Serial.println(F("step"));   
    //e' stato premuto il bottone di step a ciclo avviato
    //interrompo i timers, e passo al rosso
-   stopTimers();
-   toRed();
+   return true;
    deb = 0;
    startPush = true;
   }
+  return false;
 }
 
 
-void startButtonCtrl() {
+boolean startButtonCtrl() {
   //il debounce evita paciughi nella lettura della pressione del bottone
   static int deb =0;
   static boolean startPush = false; 
@@ -257,43 +257,21 @@ void startButtonCtrl() {
 
   if(!startPush && deb > 100) {
     //il bottone e' stato premuto
-    startPushed();
     deb = 0;
     startPush = true;
+    return true;
   } 
 
+  return false;
 }
 
-void startPushed() {
-  Serial.println(F("start premuto"));
-  
-  if(state == IDLE_STATE) {
-     Serial.println(F("START"));
-     
-     if(!digitalRead(REC_B)) {
-       //richiesta recupero accettata
-       recupero = true;
-       state = CDCICLE_STATE;
-     } else {
-       //no recupero, si passo allo stato AB
-       recupero = false;
-       state = ABCICLE_STATE;
-     }
 
-     //dopo il preTime esegue toGreen()
-     greenID = t.after(preTime, toGreen);
-     if(greenID == -1)
-      Serial.println(F("pre time error"));
-     else
-      Serial.println(F("pre time"));
-     beep(2);
-     
-  } else if(state == ABCICLE_STATE || state == CDCICLE_STATE) {
-     //premere il bottone start durante fa resetta sutto allo stato di attesa
-     Serial.println(F("RESET")); 
-     stopTimers();
-     state = IDLE_STATE;
+
+void tick() {
+  if(cycle != IDLE_CYCLE) {
+    Serial.print(++secs, DEC);
+    Serial.print(".");
   }
+ 
 }
-  
-  
+
